5037ecbbdee7c2343098e486905746025a16c22a
stephen <rguerra712@yahoo.com>
2018-09-08 12:28:19 -0500

    Added basic wait and cleaned up a little with async

--- handler.js
+++ handler.js
@@ -2,33 +2,33 @@
 
 const sourceCodeReader = require('./source-code-reader');
 
-module.exports.source = (event, context, callback) => {
+module.exports.source = async (event, context, callback) => {
   let url = event.queryStringParameters && event.queryStringParameters['url'];
+  let waitForElement = event.queryStringParameters && event.queryStringParameters['waitForElement'];
   if (!url) {
     const response = {
       statusCode: 400,
       body: 'url query string is required',
+      'Content-Type': 'text/plain',
     };
-
+    console.log(`request made with no url: ${JSON.stringify(event, null, 2)}`)
     callback(null, response);
     return;
   }
-  sourceCodeReader.getSource(url)
-    .then(source => {
+    try {
+      var source = await sourceCodeReader.getSource(url, waitForElement);
       const response = {
         statusCode: 200,
         body: source,
       };
-
       callback(null, response);
-    })
-    .catch(error => {
+    } catch (error) {
       const response = {
         statusCode: 500,
         body: error,
+        'Content-Type': 'text/plain',
       };
-
-      callback(null, response);
       console.error(error);
-    });
+      callback(null, response);
+    }
 };

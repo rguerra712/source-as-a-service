934def98725080657d92b95935df46cf9011c928
stephen <rguerra712@yahoo.com>
2018-09-08 12:37:09 -0500

    Fixed API contract to be json and a little cleaner

--- handler.js
+++ handler.js
@@ -8,8 +8,10 @@ module.exports.source = async (event, context, callback) => {
   if (!url) {
     const response = {
       statusCode: 400,
-      body: 'url query string is required',
-      'Content-Type': 'text/plain',
+      body: JSON.stringify({ error: 'url query string is required'}, null, 2),
+      headers: {
+        "content-type": "v"
+      },
     };
     console.log(`request made with no url: ${JSON.stringify(event, null, 2)}`)
     callback(null, response);
@@ -19,14 +21,19 @@ module.exports.source = async (event, context, callback) => {
       var source = await sourceCodeReader.getSource(url, waitForElement);
       const response = {
         statusCode: 200,
-        body: source,
+        body: JSON.stringify({body: source}, null, 2),
+        headers: {
+          "content-type": "application/json"
+        },
       };
       callback(null, response);
     } catch (error) {
       const response = {
         statusCode: 500,
-        body: error,
-        'Content-Type': 'text/plain',
+        body: JSON.stringify({error}, null, 2),
+        headers: {
+          "content-type": "application/json"
+        },
       };
       console.error(error);
       callback(null, response);
